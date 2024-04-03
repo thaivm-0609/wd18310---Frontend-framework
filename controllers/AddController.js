cf7a262550b1e0105d08bcb6293641b665196a71
lovent <vuongthai95@gmail.com>
2024-04-03 15:50:44 +0700

    validate

--- controllers/AddController.js
+++ controllers/AddController.js
@@ -6,11 +6,27 @@ window.AddController = function ($scope,$http,$location) {
             ...$scope.inputValue,
         }
 
-        $http.post(apiUrl,newProduct).then(function (response) {
-            console.log(response);
-            if (response.status == 201) {
-                $location.path('/list');
-            }
-        })
+        //khởi tạo biến để kiểm tra hợp lệ
+        var invalid = false;
+        //nếu trường name bỏ trống
+        if (!$scope.inputValue  
+            || !$scope.inputValue.name 
+            || $scope.inputValue.name.length < 6
+        ) {
+            invalid = true;
+        }
+
+        //nếu trường description bỏ trống
+        if (!$scope.inputValue || !$scope.inputValue.description) {
+            invalid = true;
+        }
+
+        if (!invalid) {
+            $http.post(apiUrl,newProduct).then(function (response) {
+                if (response.status == 201) {
+                    $location.path('/list');
+                }
+            })
+        }
     }
 }
